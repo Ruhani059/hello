19711cb037e5ee7821baa8c8f8da61c6a6fa0a6e
Ruhani059 <ruhani.buet.11@gmail.com>
2017-06-07 15:18:41 +0600

    Test Upload

---
+++ include/HelloWorld.h
@@ -0,0 +1,16 @@
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+
+
+class HelloWorld
+{
+    public:
+        HelloWorld();
+        virtual ~HelloWorld();
+
+    protected:
+
+    private:
+};
+
+#endif // HELLOWORLD_H
---
+++ src/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include "HelloWorld.h"
+#include <iostream>
+using namespace std;
+HelloWorld::HelloWorld()
+{
+    //ctor
+}
+
+HelloWorld::~HelloWorld()
+{
+    //dtor
+}
+
